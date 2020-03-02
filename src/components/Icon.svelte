b35a9aeaef1808c3c3660a6a645edebe59d17d8b
Yerko Palma <yerko.palma@usach.cl>
2020-03-02 09:46:39 -0300

    finished nav

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -12,12 +12,12 @@
     easing: cubicOut
   })
   export let isOpen = false
+  $: if (isOpen) {
+    lines.set([[4, 20],[12, 12], [20, 4]])
+  } else {
+    lines.set([[6, 6],[12, 12], [18, 18]])
+  }
   function toggle (e) {
-    if (isOpen) {
-      lines.set([[6, 6],[12, 12], [18, 18]])
-    } else {
-      lines.set([[4, 20],[12, 12], [20, 4]])
-    }
     isOpen = !isOpen
   }
 </script>
@@ -26,10 +26,10 @@
 <svg 
   on:mouseenter="{e => !isOpen && lines.set([[4, 4], [12, 12], [20, 20]])}"
   on:mouseleave="{e => !isOpen && lines.set([[6, 6],[12, 12], [18, 18]])}"
-  on:click="{toggle}"
   viewBox="0 0 24 24" width="{size}" height="{size}" stroke="{color}" stroke-width="{strokeWidth}" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <line x1="3" y1={$lines[0][0]} x2="21" y2={$lines[0][1]}></line>
   <line x1="3" y1={$lines[1][0]} x2={isOpen ? 3 : 21} y2={$lines[1][1]}></line>
+  <line x1={isOpen ? 21 : 3} y1={$lines[1][0]} x2="21" y2={$lines[1][1]}></line>
   <line x1="3" y1={$lines[2][0]} x2="21" y2={$lines[2][1]}></line>
 </svg>
 {/if}
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,28 +1,74 @@
 <script>
   import Icon from './Icon.svelte'
+  import { tweened } from 'svelte/motion'
+  import { cubicOut } from 'svelte/easing'
+  import { goto } from '@sapper/app'
+
+  let btn
+  const left = tweened(-300, {
+    duration: 200,
+    easing: cubicOut
+  })
+  const border = tweened(0, {
+    duration: 200,
+    easing: cubicOut
+  })
+  let color = '#333'
+  let isOpen = false
   export let segment
+
+  const toggle = (e) => {
+    if (color === '#333') {
+      left.set(0)
+      border.set(2)
+      color= '#eee'
+    } else {
+      left.set(-300)
+      border.set(0)
+      color= '#333'
+    }
+    isOpen = !isOpen
+  }
+
+  const navigate = async (route) => {
+    await goto(route)
+    btn.click()
+  }
 </script>
 
 <style>
   nav {
     font-weight: 300;
-    padding: 0 1em;
+    position: fixed;
+    width: 100%;
+    display: block;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    padding: 0;
   }
 
   button {
     background: transparent;
     border: none;
     padding: .5rem;
-      cursor: pointer;
+    cursor: pointer;
+    z-index: 100;
+    position: absolute;
+    margin-left: 1rem;
+    outline: none;
   }
 
   ul {
     margin: 0;
-    padding: 0;
+    padding: 3.5rem 0;
     position: fixed;
-    width: 20%;
+    width: 10%;
     top: 0;
     left: -20%;
+    height: 100%;
+    z-index: 100;
+    background-color: #333;
   }
 
   /* clearfix */
@@ -42,19 +88,45 @@
     text-decoration: none;
     padding: 1em 0.5em;
     display: block;
+    color: #eee;
+    font-size: 1em;
+    cursor: pointer;
+    text-transform: uppercase;
+    border-top: 1px #222 solid;
+  }
+  li:last-child a {
+    border-bottom: 1px #222 solid;
+  }
+  .glass {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, .4);
   }
 </style>
 
-<button>
-  <Icon name="menu" size="30"></Icon>
+<button on:click="{toggle}" bind:this={btn}>
+  <Icon name="menu" size="40" color={color} isOpen={isOpen}></Icon>
 </button>
-<nav>
-  <ul>
-    <li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
-    <li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
+<nav style="border: #333 solid {$border}rem; border-left: #333 solid {$border * 4}rem;">
+  <ul style="left:{$left}px;">
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <li>
+      <a on:click|preventDefault="{e => navigate('.')}">home</a>
+    </li>
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <li>
+      <a on:click|preventDefault="{e => navigate('about')}">about</a>
+    </li>
 
     <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
          the blog data when we hover over the link or tap it on a touchscreen -->
-    <li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <li>
+      <a rel=prefetch on:click|preventDefault="{e => navigate('blog')}">blog</a>
+    </li>
   </ul>
+  {#if color !== '#333'}
+    <div on:click="{toggle}" class="glass"></div>
+  {/if}
 </nav>
